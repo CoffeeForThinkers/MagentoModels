4078cdb52a66beb18c75207ecbde6363e97dbccb
Dustin Oprea <dustin@ip-10-0-1-5.ec2.internal>
2016-03-19 08:48:11 +0000

    Swithed which stored procedure.

--- mm/routines/magento/sales_inventory.py
+++ mm/routines/magento/sales_inventory.py
@@ -15,6 +15,19 @@ class SalesInventoryRoutines(mm.routines.RoutinesBase):
                 start_timestamp, stop_timestamp)
         
         for record in records:
-            record['quantity'] = float(record['quantity'])
+            record['quantity'] = int(record['quantity'])
 
             yield record
+
+    def get_sales_with_items(self, start_timestamp, stop_timestamp):
+        records = \
+            self.call(
+                'get_sales_with_items',
+                start_timestamp, stop_timestamp)
+
+        for record in records:
+            record['quantity'] = int(record['quantity'])
+            record['order_number'] = int(record['order_number'])
+
+            yield record
+
