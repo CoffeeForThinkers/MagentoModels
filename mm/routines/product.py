0cdeab0eb0062cd1f5a0d88b99a23d619a67f936
diegot <diego.terzano@gmail.com>
2016-05-19 10:37:04 -0400

    Fixed issue with identity checking on input validation.

--- mm/routines/product.py
+++ mm/routines/product.py
@@ -20,15 +20,18 @@ class ProductRoutines(mm.routines.RoutinesBase):
         legal_inputs = set([None, True, False])
         message = "Not a valid input value for '{0}'. Use: {1}"
 
-        assert type(store_id) is int or store_id is None, message.format('store_id', 'None or int')
-        assert is_active in legal_inputs, message.format('is_active', 'None, True or False')
-        assert is_visible in legal_inputs, message.format('is_visible', 'None, True or False')
+        assert type(store_id) is int or store_id is None, \
+            message.format('store_id', 'None or int')
+        assert is_active is True or is_active is False or is_active is None, \
+            message.format('is_active', 'None, True or False')
+        assert is_visible is True or is_visible is False or is_visible is None, \
+            message.format('is_visible', 'None, True or False')
 
         rows = \
             self.call(
                 'get_configurable_associated_products',
                 store_id,
-                is_active,
-                is_visible)
+                int(is_active),
+                int(is_visible))
 
         return rows
